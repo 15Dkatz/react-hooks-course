a10446a8f8d7bd9deda8faa742659efc0a1baac9
David Katz <15Dkatz@shcp.edu>
2019-02-18 00:07:07 -0800

    Complete section 1

--- home/src/App.js
+++ home/src/App.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import Joke from './Joke';
+import Stories from './Stories';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        App
-      </div>
-    );
+function App() {
+  const [userQuery, setUserQuery] = useState('');
+
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery);
+    setUserQuery(event.target.value);
   }
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  }
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello David</h1>
+      <div className='form'>
+        <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress} />
+        <button onClick={searchQuery}>Search</button>
+      </div>
+      <hr />
+      <Joke />
+      <hr />
+      <Stories />
+    </div>
+  );
 }
 
 export default App;
---
+++ home/src/hooks.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+export const useFetch = (url, initialValue) => {
+  const [result, setResult] = useState(initialValue);
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(json => setResult(json));
+  }, []);
+
+  return result;
+}
